72be06007de339b058a48e557391dbdebaf1e990
robinristo78 <robinristo@gmail.com>
2025-09-10 11:35:04 +0300

    Week 1 & 2 - Exercises 1, 5, 26, 29

---
+++ src/week1_exercise1/main.java
@@ -0,0 +1,15 @@
+package week1_exercise1;
+
+//Exercise 1: Name
+
+public class main {
+    public static void main(String[] args) {
+        // We will print the text "Hello world"
+        //System.out.print("Hello world");
+
+        //System.out.print(" and all the people of the world."); // We print more text to the same line.
+
+        // System.out.print("this line will not be executed, because it is commented out");
+        System.out.println("Jane Doe");
+    }
+}
